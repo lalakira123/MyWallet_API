d08c3fbcd0e2199e554c0d0e622c38493e17a007
Viviane Brenner <brenner.vivi@gmail.com>
2022-05-20 15:06:51 -0300

    Preparando revisão de código

--- schemas/loginUserSchema.js
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-
-const loginUserSchema = Joi.object({
-    email: Joi.string().trim().required(),
-    password: Joi.string().trim().required()
-});
-
-export default loginUserSchema;
--- schemas/postMovementsSchema.js
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-const postMovementsSchema = Joi.object({
-    movement: Joi.number().required(),
-    description: Joi.string().trim().required(),
-    isPlus: Joi.boolean()
-})
-
-export default postMovementsSchema;
